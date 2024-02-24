e37a0463e1b6c90262905ed2dead951e268f7f37
AashiqRahaman <aashiqrahaman0@gamil.com>
2024-02-24 23:54:16 +0530

    some fun project file

---
+++ SomeFunWork/group.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+# Hi_I_am_Aashiq_CSE_Student
+time.sleep(3)
+count = 0
+
+while count <= 50 : 
+    pyautogui.typewrite("#Change_Our_Teacher #We_Don't_Tollarate_Harassment")
+    pyautogui.hotkey('enter')
+    count = count + 1
+    time.sleep(0.8)
+
+    
