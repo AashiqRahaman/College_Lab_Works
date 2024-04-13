07a067d8f0b9890d3ca622b2710ce91877b0fe4e
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-04-13 10:40:31 +0530

    fibo nacci

--- Testing_Files/test.py
+++ Testing_Files/test.py
@@ -1,1 +1,15 @@
-print("testing for check wheather contribution counting or not ")
+def even_fibonacci_sum(limit):
+    a, b = 1, 2
+    total_sum = 0
+    
+    while a <= limit:
+        print(a)
+        if a % 2 == 0:
+            total_sum += a
+        a, b = b, a + b
+    
+    return total_sum
+
+# Calculate the sum of even-valued Fibonacci terms below 4 million
+result = even_fibonacci_sum(10)
+print(result)
