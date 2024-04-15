140cddb50170b174c50c0a29eb18918f41742eed
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-04-15 23:56:32 +0530

    c file added

---
+++ C_Programming_Lab_Works/LAB DAY 4/Q3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void perfect(int);
+void main()
+{
+    int n;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+    perfect(n);
+}
+void perfect(int n)
+{   int p, q;
+    for (p = 1; p <= n; p++)
+    {   int sum = 0;
+        for (q = 1; q < p; q++)
+        { if (p % q == 0)
+            {
+                sum = sum + q;
+            }
+        }
+        if (sum == p)
+        {
+            printf("%d ", p);
+        }
+    }
+}
