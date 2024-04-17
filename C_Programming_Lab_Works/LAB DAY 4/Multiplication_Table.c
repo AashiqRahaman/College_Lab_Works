d5dac43c51554dc09f9080e01d2868396d3bd623
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-04-17 19:44:15 +0530

    name change

--- C_Programming_Lab_Works/LAB DAY 4/Multiplication_Table.c
+++ C_Programming_Lab_Works/LAB DAY 4/Multiplication_Table.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {   int n, o, i, j;
-    printf("Enter Your Input Number For Table:  ");
+    printf("Enter Your Input Number For Whose Table Y19ou Want:  ");
     scanf("%d", &n);
     printf("Enter Upto how Long Table You Want:  ");
     scanf("%d", &o);
