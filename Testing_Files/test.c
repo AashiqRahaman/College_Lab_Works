e12ebf70c4b607fdd253761b6750f7bd2b6952d5
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-05-06 05:23:13 +0530

    test

--- Testing_Files/test.c
+++ Testing_Files/test.c
@@ -2,5 +2,13 @@
 
 int main ()
 {
-    printf("Test Files");
+    int arr[5];
+    for (int i=0;i<4;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (int i=0;i<4;i++)
+    {
+        printf("%d",arr[i]);
+    }
 }
