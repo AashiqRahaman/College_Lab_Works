395993cf4accbd11e35852d7abdd10d0f3cab4bd
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-05-09 00:07:30 +0530

    cfile added

---
+++ C_Programming_Lab_Works/LAB DAY 6/Q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+    int c, i, j, k = 1, l = 2;
+    printf("Ener Your Column Number :- ");
+    scanf("%d", &c);
+    for (i = 1; i <= c; i++)
+    {
+        for (j = c; j >= i; j--)
+        {
+            if (i == 1)
+            {
+                printf("%d", k);
+                k++;
+            }
+            else if (j == i)
+            {
+                printf("%d", c);
+            }
+            else if (j == c)
+            {
+                printf("%d", l);
+                l++;
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
