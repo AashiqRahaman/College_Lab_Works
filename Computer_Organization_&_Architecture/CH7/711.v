ee7d8e7ba90d56e44de1a52a19c2e09c6acce8a0
AashiqRahaman <aashiqrahaman0@gmail.com>
2024-05-22 12:18:15 +0530

    changes

---
+++ Computer_Organization_&_Architecture/CH7/711.v
@@ -0,0 +1,18 @@
+// 7.1.1 How does a Verilog model for a D-flip-flop han-dle treating reset as the highest priority input?
+
+
+module dff (
+  input clk,
+  input reset,
+  input D,
+  output reg q
+);
+
+  always @(posedge clk or negedge reset) begin
+    if (!reset) begin 
+      q <= 0; 
+    end else begin 
+      q <= d;
+    end
+  end
+endmodule
