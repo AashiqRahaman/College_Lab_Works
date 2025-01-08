f757dccf71a46da0ae1f85b71ac754689374ab93
AashiqRahaman <aashiqrahaman0@gmail.com>
2025-01-08 11:23:50 +0530

    ch orientation for printing

--- DBMS/lab/lab3.sql
+++ DBMS/lab/lab3.sql
@@ -1,27 +1,18 @@
 #Extras
 
 #1.Find Out staff Details who work for Same Branch which Have more than 1 Employee.
-SELECT *
-FROM Staff
-WHERE branchNo IN (SELECT s.branchNo
-FROM Staff s
-Group BY branchNo
-HAVING COUNT(staffNo) > 1);
+SELECT * FROM Staff WHERE branchNo IN (SELECT s.branchNo FROM Staff s
+Group BY branchNo HAVING COUNT(staffNo) > 1);
 
 #2.Create a View Which Display BranchNo,StaffNo.Position & ClientNo
-CREATE VIEW custom AS
-Select s.branchNo,s.staffNo,s.position,c.clientNo
-From Staff s
-JOIN Registration c ON s.staffNo = c.staffNo;
-
+CREATE VIEW custom AS Select s.branchNo,s.staffNo,s.position,c.clientNo
+From Staff s JOIN Registration c ON s.staffNo = c.staffNo;
 Select * FROM custom;
 
 #3.SHOW all details from Cross Product of Staff,Registration
-SELECT *
-FROM Staff,Registration;
+SELECT * FROM Staff,Registration;
 
 #4.Find the Street & city of the Branch for ALL Clients whose Max Rent > 400
-SELECT c.clientNo,c.fName,c.lName,b.branchNo, b.street,b.city
-FROM Client c
+SELECT c.clientNo,c.fName,c.lName,b.branchNo, b.street,b.city FROM Client c
 JOIN Registration r ON c.clientNo = r.clientNo AND maxRent > 400
 JOIN Branch b ON r.branchNo = b.branchNo;
